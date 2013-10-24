4687db862096781ad931fca5d6dfd8cbdef104fa
Mark Wunsch <mark@markwunsch.com>
2013-10-24 17:33:17 -0400

    Basic headline and article construction

--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+set :revision, `git rev-parse --short HEAD`
+set :headlines, ["Spinning Newspaper Injures Printer"]
+
 get '/' do
-  erb :paper
+  headline = params[:q] || settings.headlines.sample
+  erb :paper, locals: { headline: headline, revision: settings.revision }
 end
