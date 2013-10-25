b3cc70689522cec614f9ba01e2def584452c852f
Mark Wunsch <mark@markwunsch.com>
2013-10-25 13:37:22 -0400

    Make it an application!

--- Gemfile
+++ Gemfile
@@ -1,4 +1,8 @@
 ruby '2.0.0'
+source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'puma'
+gem 'sass'
+gem 'bourbon'
+gem 'literate_randomizer'
--- app.rb
+++ app.rb
@@ -1,9 +1,28 @@
 require 'sinatra'
+require 'csv'
+require 'digest/md5'
+require 'literate_randomizer'
 
 set :revision, `git rev-parse --short HEAD`
-set :headlines, ["Spinning Newspaper Injures Printer"]
+set :cache, {}
+set :headlines, CSV.read(File.join(settings.root, "headlines.csv"), headers: true)["Text"].concat([
+    "Spinning Newspaper Injures Printer",
+    "Squirrel Resembling Abraham Lincoln Found",
+    "Cosmetics Scare In Gotham"
+  ])
 
 get '/' do
-  headline = params[:q] || settings.headlines.sample
-  erb :paper, locals: { headline: headline, revision: settings.revision }
+  headline = (params[:q] || settings.headlines.sample).strip
+  checksum = Digest::MD5.hexdigest(headline)
+  settings.cache.fetch(checksum) do |sum|
+    view = erb :paper, locals: {
+      headline: headline,
+      checksum: sum,
+      revision: settings.revision,
+      paragraphs: LiterateRandomizer.paragraphs(join: false, paragraphs: 12..18)
+    }
+    settings.cache[sum] = view if params[:q]
+    view
+  end
 end
+
